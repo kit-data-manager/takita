70dcb9871a1e2ead162f90ff1427a6ea38d5099f
Maximilian Walz <utokx@student.kit.edu>
2020-08-16 15:54:34 +0200

    Add editor_stub html, add strings for it, implement create anno frontend

--- tuhl/src/main/java/edu/kit/scc/dem/tuhl/editorstub/EditorStubController.java
+++ tuhl/src/main/java/edu/kit/scc/dem/tuhl/editorstub/EditorStubController.java
@@ -1,22 +1,52 @@
 package edu.kit.scc.dem.tuhl.editorstub;
 
+import edu.kit.scc.dem.tuhl.NoSuchIndexEntryException;
+import edu.kit.scc.dem.tuhl.model.Annotation;
+import edu.kit.scc.dem.tuhl.model.Color;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @Controller
+@RequestMapping("/editor_stub")
 public class EditorStubController {
 
-  private IEditorStubService editorStubService;
+  private final IEditorStubService editorStubService;
 
+  @Autowired
   public EditorStubController(IEditorStubService editorStubService) {
     this.editorStubService = editorStubService;
+  }
+  
+  @RequestMapping
+  public String init() {
+    return "editor_stub";
   }
 
   /*
   Needs: pageId
   Can get: color, svgCode, motivation
    */
-  public String addAnnotation() {
-    throw new AssertionError("Not implemented yet");
+  @PostMapping("/add_annotation")
+  public String addAnnotation(@RequestBody String jsonString,
+                              Model model) {
+    try {
+      JSONObject json = new JSONObject(jsonString);
+      String pageId = json.getString("pageId");
+      String color = json.getString("color");
+      String svgCode = json.getString("svgCode");
+      String motivation = json.getString("motivation");
+      Annotation annotation = editorStubService.addAnnotation(pageId, null, svgCode, motivation);
+      model.addAttribute("annotation", annotation);
+    } catch (JSONException | InterruptedException | IOException | NoSuchIndexEntryException e) {
+      return "redirect:/error/" + e.getMessage();
+    }
+    return "editor_stub :: annotationViewer";
   }
 
   /*
