318941755c64a5983d5b25d89c1bb7665a02a6b5
Nico <nicoletta.puetz@web.de>
2020-08-30 18:56:29 +0200

    Fix editor Stub Controller Tests

--- tuhl/src/test/java/edu/kit/scc/dem/tuhl/editorstub/EditorStubControllerTest.java
+++ tuhl/src/test/java/edu/kit/scc/dem/tuhl/editorstub/EditorStubControllerTest.java
@@ -92,7 +92,7 @@ class EditorStubControllerTest {
     String valid = "{\"pageId\":\"2c01883b-5aae-4867-b0f2-06fbb093f61e\",\"color\":\"TEXT_REGION\",\"svgCode\":\"\",\"motivation\":\"EDITING\"}";
     this.mockMvc.perform(post("/editor_stub/create_annotation").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: annotationViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_ANNOTATION_VIEWER))
         .andExpect(model().attribute("annotation", equalTo(mockAnno)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -111,7 +111,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"http://192.168.0.49:8100/wap/a04/validated/065c57d9-3800-49f8-838a-11a0455d8f60\"}";
     this.mockMvc.perform(post("/editor_stub/read_annotation").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: annotationViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_ANNOTATION_VIEWER))
         .andExpect(model().attribute("annotation", equalTo(mockAnno)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -130,7 +130,7 @@ class EditorStubControllerTest {
     String valid = "{\"annoId\":\"http://192.168.0.49:8100/wap/a04/validated/78ee189a-5ac9-4b1c-ac0b-49863387fc49\",\"color\":\"TEXT_REGION\",\"svgCode\":\"\",\"motivation\":\"BOOKMARKING\"}";
     this.mockMvc.perform(post("/editor_stub/update_annotation").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: annotationViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_ANNOTATION_VIEWER))
         .andExpect(model().attribute("annotation", equalTo(mockAnno)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -148,7 +148,7 @@ class EditorStubControllerTest {
     String valid = "{\"annoId\":\"http://192.168.0.49:8100/wap/a04/validated/065c57d9-3800-49f8-838a-11a0455d8f60\"}";
     this.mockMvc.perform(post("/editor_stub/delete_annotation").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: annotationViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_ANNOTATION_VIEWER))
         .andExpect(model().attribute("annotation", equalTo(mockAnno)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -167,7 +167,7 @@ class EditorStubControllerTest {
     String valid = "{\"annoId\":\"http://192.168.0.49:8100/wap/a04/validated/065c57d9-3800-49f8-838a-11a0455d8f60\"}";
     this.mockMvc.perform(post("/editor_stub/validate_annotation").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: annotationViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_ANNOTATION_VIEWER))
         .andExpect(model().attribute("annotation", equalTo(mockAnno)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -186,7 +186,7 @@ class EditorStubControllerTest {
     String valid = "{\"annoId\":\"http://192.168.0.49:8100/wap/a04/validated/78ee189a-5ac9-4b1c-ac0b-49863387fc49\",\"title\":\"Ich bin ein Titel\",\"purpose\":\"DESCRIBING\",\"value\":\"Ich bin der Value\"}";
     this.mockMvc.perform(post("/editor_stub/create_card").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockCard)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -205,7 +205,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"3fc86c30-5ce6-4ab3-8076-6c3109f61384\"}";
     this.mockMvc.perform(post("/editor_stub/read_card").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockCard)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -224,7 +224,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"3fc86c30-5ce6-4ab3-8076-6c3109f61384\",\"title\":\"Title\",\"purpose\":\"HIGHLIGHTING\",\"value\":\"Value\"}";
     this.mockMvc.perform(post("/editor_stub/update_card").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockCard)))
         .andDo(MockMvcResultHandlers.print());
 
@@ -243,7 +243,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"3fc86c30-5ce6-4ab3-8076-6c3109f61384\"}";
     this.mockMvc.perform(post("/editor_stub/delete_card").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockCard)))
         .andDo(MockMvcResultHandlers.print());
   }
@@ -261,7 +261,7 @@ class EditorStubControllerTest {
     String valid = "{\"annoId\":\"http://192.168.0.49:8100/wap/a04/validated/78ee189a-5ac9-4b1c-ac0b-49863387fc49\",\"title\":\"Taggg\",\"value\":\"valuee\"}";
     this.mockMvc.perform(post("/editor_stub/create_tag").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockTag)))
         .andDo(MockMvcResultHandlers.print());
 
@@ -280,7 +280,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"dc66e069-9774-4525-a901-03cdcfa36af9\"}";
     this.mockMvc.perform(post("/editor_stub/read_tag").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockTag)))
         .andDo(MockMvcResultHandlers.print());
 
@@ -299,7 +299,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"dc66e069-9774-4525-a901-03cdcfa36af9\",\"title\":\"taag\",\"value\":\"vvvalue\"}";
     this.mockMvc.perform(post("/editor_stub/update_tag").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockTag)))
         .andDo(MockMvcResultHandlers.print());
 
@@ -319,7 +319,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"dc66e069-9774-4525-a901-03cdcfa36af9\"}";
     this.mockMvc.perform(post("/editor_stub/delete_tag").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: bodyViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_BODY_VIEWER))
         .andExpect(model().attribute("body", equalTo(mockTag)))
         .andDo(MockMvcResultHandlers.print());
 
@@ -341,7 +341,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"000073cd-c425-4214-9648-b380ff20c61a\"}";
     this.mockMvc.perform(post("/editor_stub/raw_manuscript_json").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: rawJsonViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_RAW_JSON_VIEWER))
         .andExpect(model().attribute("rawJson", equalTo(mockObj.toString(2).replace("\\/", "/"))))
         .andDo(MockMvcResultHandlers.print());
 
@@ -361,7 +361,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"000073cd-c425-4214-9648-b380ff20c61a\"}";
     this.mockMvc.perform(post("/editor_stub/raw_manuscript_xml").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: rawXmlViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_RAW_XML_VIEWER))
         .andDo(MockMvcResultHandlers.print());
 
   }
@@ -382,7 +382,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"2c01883b-5aae-4867-b0f2-06fbb093f61e\"}";
     this.mockMvc.perform(post("/editor_stub/raw_page_json").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: rawJsonViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_RAW_JSON_VIEWER))
         .andExpect(model().attribute("rawJson", equalTo(mockObj.toString(2).replace("\\/", "/"))))
         .andDo(MockMvcResultHandlers.print());
 
@@ -403,7 +403,7 @@ class EditorStubControllerTest {
     String valid = "{\"id\":\"http://192.168.0.49:8100/wap/a04/validated/065c57d9-3800-49f8-838a-11a0455d8f60\"}";
     this.mockMvc.perform(post("/editor_stub/raw_annotation_json").contentType(MediaType.APPLICATION_JSON).content(valid))
         .andExpect(status().isOk())
-        .andExpect(view().name("editor_stub :: rawJsonViewer"))
+        .andExpect(view().name(EditorStubController.FRAGMENT_RAW_JSON_VIEWER))
         .andExpect(model().attribute("rawJson", equalTo(mockObj.toString(2).replace("\\/", "/"))))
         .andDo(MockMvcResultHandlers.print());
 
