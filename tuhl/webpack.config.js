84c58c066dedfcd3c7ffd143be8cfda6d22444f4
Henning Gebhard <henning.gebhard@rub.de>
2023-11-20 11:36:42 +0100

    setup javascript toolchain
    This commit includes a set of config files to make use of
    
    - node (package management)
    - babel (transpiling modern ES6 into universally compatible legacy JS)
    - eslint (linting)
    - prettier (set of linting rules)
    - jest (testing)
    
    As is, you can now use `npm i` from the `tuhl/` directory to fetch and
    install all needed dependencies (or `npm ci` if you want to install
    the exact versions which are specified in package-lock.json).

---
+++ tuhl/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+//const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+// This is the path where webpack expects to find all the js files
+// which it should bundle up.
+const srcPath = path.resolve(__dirname, './src/main/resources/js-src');
+
+// This is the path where webpack will place the built bundles. There
+// will be one bundle for every `entry` which is defined below.
+const dstPath = path.resolve(__dirname, './src/main/resources/static');
+
+// Add your new app name and its entry point to the `entry` object,
+// so that webpack picks it up, builds it and copies the resulting
+// bundle into our regular static files folder.
+module.exports = {
+  entry: {
+    'js/analysis.js': path.resolve(srcPath, './analysis/index.js'),
+    //'css/analysis.css': path.resolve(srcPath, './analysis/index.css'),
+    'js/texteditor.js': path.resolve(srcPath, './texteditor/index.js'),
+  },
+  output: {
+    path: path.resolve(dstPath),
+    filename: './[name]',
+  },
+  externals: {
+    //react: 'React',
+    //'react-dom': 'ReactDOM',
+    lodash: 'lodash',
+    quill: 'quill',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [new ESLintPlugin()],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
