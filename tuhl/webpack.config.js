18b572d25b392a7d45fa94243ebe651c09797b3d
Henning Gebhard <henning.gebhard@rub.de>
2023-12-15 11:25:42 +0100

    exclude all entry points for now

--- tuhl/webpack.config.js
+++ tuhl/webpack.config.js
@@ -16,9 +16,9 @@ const dstPath = path.resolve(__dirname, './src/main/resources/static');
 // bundle into our regular static files folder.
 module.exports = {
   entry: {
-    'js/analysis.js': path.resolve(srcPath, './analysis/index.js'),
+    //'js/analysis.js': path.resolve(srcPath, './analysis/index.js'),
     //'css/analysis.css': path.resolve(srcPath, './analysis/index.css'),
-    'js/texteditor.js': path.resolve(srcPath, './texteditor/index.js'),
+    //'js/texteditor.js': path.resolve(srcPath, './texteditor/index.js'),
   },
   output: {
     path: path.resolve(dstPath),
