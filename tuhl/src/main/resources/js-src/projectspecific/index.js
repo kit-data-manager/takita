acaeac2f588d1d5c2b918fd0afe0282fdb32a799
ro2140 <philipp.toegel@kit.edu>
2024-06-25 13:38:23 +0200

    improving documentation

--- tuhl/src/main/resources/js-src/projectspecific/index.js
+++ tuhl/src/main/resources/js-src/projectspecific/index.js
@@ -13,7 +13,7 @@ import { defaultHighlighting } from '../texteditor-ng/highlighting';
 import { updateTargetAndBodyData } from './data';
 import { crc1475Highlighting, getSpecificClasses } from './highlight';
 // EXPORTS
-// mandatory exports
+// mandatory exports (optional exports can be found further down)
 export { targetUpdateCallback, POSSIBLE_DIVISION_TYPES, highlightAnnotationFunction, possibleHighlightClasses };
 export { hooks } from './hooks';
 export { formObjectCreateAnnotation, formObjectCreateBody } from './annotationCreation';
@@ -21,12 +21,16 @@ export { headerFieldsArray, omitFieldsArray, editableFieldsArray } from './annot
 export { Variant } from './textloader';
 export { getColorHexFromEnumEntry, getColorNameFromEnumEntry } from './utils';
 
-// TODO: CUSTOMIZE to be the correct function for your project case
+// TODO: CUSTOMIZE to be the correct function for your project case. The function is called
+// when the buttons appended to the annotationCard are used to update the target of an annotation
+// targetting text (common/annotationCard/annotationCard.js).
 // - updateTargetData is the standard function to update a target, it will only update the target
 // - updateTargetAndBodyData is the function used by CRC1475 to update the target and the body, which
 //   stores the selected text (describing body)
 const targetUpdateCallback = updateTargetAndBodyData;
 
+// TODO: CUSTOMIZE the divisions used in the project for dividing texts into chapters, sections. etc.
+// This is used by the navigation module.
 // TODO: move "subchapter" to the second navigation level
 const POSSIBLE_DIVISION_TYPES = ['chapter', 'section', 'subchapter'];
 
@@ -37,14 +41,21 @@ const POSSIBLE_DIVISION_TYPES = ['chapter', 'section', 'subchapter'];
  * - crc1475Highlighting is the function used by CRC1475 to highlight all targets of an annotation.
  *   It assigns classes responsible for background colors and underlinings for the different
  *   annotation types.
+ * The function is called at "drawAnnos" in texteditor-ng/highlight/target.js.
  */
 const highlightAnnotationFunction = crc1475Highlighting;
 
 // add the "defaulthighlighting" class to the project specific classes. The project specific classes
 // get "fetched" by calling the getProjectSpecificClasses function, that can be customized
 // (see the import statements).
+// This is used by the highlight module (texteditor-ng/highlight/target.js).
 const possibleHighlightClasses = ['defaulthighlight'].concat(getSpecificClasses());
 
+// optional exports
+// add your things here
+/**
+ * Declares global variables
+ */
 export function initializeCRC1475Specifics() {
   // selectedText stores the selected test as a string
   // it is needed to add it to the annotations body
