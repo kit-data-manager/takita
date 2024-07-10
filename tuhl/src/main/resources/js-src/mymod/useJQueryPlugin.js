d005266baa5f5a7055464fba725bf220955817af
Henning Gebhard <henning.gebhard@rub.de>
2024-07-10 16:46:20 +0200

    include super-minimalistic metadataeditor import tests

---
+++ tuhl/src/main/resources/js-src/mymod/useJQueryPlugin.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+import 'jsonform';
+import '../common/utils/metadataeditor';
+
+/**
+ *
+ * @param {HTMLNode} node
+ * @returns
+ */
+export const useJQueryPlugin = (node) => {
+  return $(node);
+};
