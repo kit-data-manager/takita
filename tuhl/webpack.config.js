b48d68bc8c20aec592da66e6664633120c910e38
ro2140 <philipp.toegel@kit.edu>
2024-07-11 13:29:01 +0200

    adding a build path for the jQuery plugin tests to test the influence of webpacks bundling on the existence of jQuery plugins

--- tuhl/webpack.config.js
+++ tuhl/webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
     'js/analysis.js': path.resolve(srcPath, './analysis/index.js'),
     //'css/analysis.css': path.resolve(srcPath, './analysis/index.css'),
     //'js/texteditor.js': path.resolve(srcPath, './texteditor/index.js'),
+    // jquery/jsonForm test module:'js/jQueryPluginTest.js': path.resolve(srcPath, './jQueryPluginTest/index.js'),
     'js/texteditor-ng.js': path.resolve(srcPath, './texteditor-ng/index.js'),
   },
   output: {
