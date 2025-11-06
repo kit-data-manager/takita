f0bee7171f996ee85f86364a41926782470670d5
Philipp Tögel <philipp.toegel@kit.edu>
2025-11-06 09:16:38 +0100

    working version with need of security fixes

--- tuhl/src/main/java/edu/kit/datamanager/takita/configuration/SecurityConfiguration.java
+++ tuhl/src/main/java/edu/kit/datamanager/takita/configuration/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
@@ -37,6 +39,8 @@ public class SecurityConfiguration {
                         .requestMatchers("/unauthenticated", "/oauth2/**", "/login/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                // disabling csrf protection for now to fix "Invalid CSRF token found"
+                .csrf(csrf -> csrf.disable())
                 // logout taken from: https://www.baeldung.com/spring-boot-keycloak
                 .logout((logout) -> {
                     var logoutSuccessHandler =
@@ -48,4 +52,12 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // overriding firewall to fix "Rejecting request due to: The request was rejected because the URL contained a potentially malicious String "%25""
+        // see https://stackoverflow.com/questions/74146917/spring-security-the-request-was-rejected-because-the-url-contained-a-potential
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedPercent(true);
+        return (web) -> web.httpFirewall(firewall);
+    }
 }
