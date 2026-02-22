37d01c357b5d7c532cc3cbfb62b8ea714f620d1f
germaine <germaine.goetzelmann@kit.edu>
2026-02-22 17:39:19 +0100

    Splitting project in frontend and backend folders

---
+++ frontend/js-src/texteditor-ng/display/display.test.js
@@ -0,0 +1,57 @@
+import { updateDisplay } from './index';
+import * as data from '../../common/data/annotations';
+
+// Silence console.xxx() for the duration of these tests, so it does
+// not spam our console
+beforeEach(() => {
+  console.error = jest.fn(() => {});
+  console.log = jest.fn(() => {});
+});
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('updating the display by rendering the current annotations', () => {
+  beforeEach(() => {
+    window.ANNOJSON = undefined;
+    jest.clearAllMocks();
+  });
+  it('successfully updates the display and store the current annotations in a variable', async () => {
+    // TODO: remove the annotation table related requirement (the two divs and the creator from the
+    // mockAnnoKson) after the display update is decoupled from the update of the annotation table
+    document.body.innerHTML = `<div id="table"></div><div id="TEI"><div id="w.1"></div><div id="w.2"></div><div id="w.3"></div>
+        <div id="w.4"></div><div id="w.5" class="defaulthighlight"></div></div>
+        <div id="annotationTableBottom"></div>
+        <div class="card card-body row-gap-2 invisible" id="annotationCard">`;
+    const mockAnnoJson = [
+      {
+        id: '1',
+        targets: [{ selector: { type: 'XPathSelector', value: ['id("w.1")', 'id("w.2")'] } }],
+        creator: 'test',
+      },
+      {
+        id: '2',
+        targets: [{ selector: { type: 'XPathSelector', value: ['id("w.3")', 'id("w.4")'] } }],
+        creator: 'test',
+      },
+    ];
+    // mocking an inner funciton call, which does a network request, to be called successfully
+    // and return the data
+    jest.spyOn(data, 'getAllAnnotationsData').mockReturnValue(mockAnnoJson);
+
+    await updateDisplay({});
+    expect(window.ANNOJSON).toStrictEqual(mockAnnoJson);
+    expect(document.getElementById('w.1').classList.contains('defaulthighlight')).toBe(true);
+    expect(document.getElementById('w.2').classList.contains('defaulthighlight')).toBe(true);
+    expect(document.getElementById('w.3').classList.contains('defaulthighlight')).toBe(true);
+    expect(document.getElementById('w.4').classList.contains('defaulthighlight')).toBe(true);
+    expect(document.getElementById('w.5').classList.contains('defaulthighlight')).toBe(false);
+  });
+  it('fails on updating the display', async () => {
+    // mocking an inner funciton call, which does a network request, to be called unsuccessfully
+    // and to throw an error, which gets handled
+    jest.spyOn(data, 'getAllAnnotationsData').mockReturnValue(new Error());
+    await updateDisplay({});
+    expect(window.ANNOJSON).toStrictEqual(undefined);
+  });
+});
---
+++ frontend/js-src/texteditor-ng/highlighting/target.test.js
@@ -0,0 +1,79 @@
+import { defaultHighlighting, removeStyles, highlightSelectedAnnotationsTarget } from './index';
+
+describe('removing all css classes relevant for highlighting a target', () => {
+  it('removes relevant classes from an element with only one child', () => {
+    const $parent = document.createElement('div');
+    $parent.innerHTML = `<div id="childA" class="a b c"></div>`;
+    const relevantClasses = ['a', 'b'];
+    const $element = $parent.querySelector('#childA');
+    removeStyles($parent, relevantClasses);
+
+    expect($element.classList.contains('a')).toBe(false);
+    expect($element.classList.contains('b')).toBe(false);
+    expect($element.classList.contains('c')).toBe(true);
+  });
+
+  it('removes relevant classes from an element with multiple children', () => {
+    const $parent = document.createElement('div');
+    $parent.innerHTML = `<div id="childA" class="a b c"></div><div id="childB" class="a d e"></div>`;
+    const relevantClasses = ['a', 'b'];
+    const $childA = $parent.querySelector('#childA');
+    const $childB = $parent.querySelector('#childB');
+    removeStyles($parent, relevantClasses);
+
+    expect($childA.classList.contains('a')).toBe(false);
+    expect($childA.classList.contains('b')).toBe(false);
+    expect($childA.classList.contains('c')).toBe(true);
+
+    expect($childB.classList.contains('a')).toBe(false);
+    expect($childB.classList.contains('d')).toBe(true);
+    expect($childB.classList.contains('e')).toBe(true);
+  });
+});
+
+describe('highlighting elements targeted by an annotation in the default way', () => {
+  it('assigns the "defaulthighlight" class to elements targeted by an annotation', () => {
+    document.body.innerHTML = `<div id="w.1"></div><div id="w.2"></div><div id="w.3"></div>`;
+    const annotation = { targets: [{ selector: { type: 'XPathSelector', value: ['id("w.1")', 'id("w.2")'] } }] };
+    const $element1 = document.getElementById('w.1');
+    const $element2 = document.getElementById('w.2');
+    const $element3 = document.getElementById('w.3');
+
+    defaultHighlighting(annotation);
+
+    expect($element1.classList.contains('defaulthighlight')).toBe(true);
+    expect($element2.classList.contains('defaulthighlight')).toBe(true);
+    expect($element3.classList.contains('defaulthighlight')).toBe(false);
+  });
+});
+
+describe('highlighting elements targeted by an annotation, which got selected by a user', () => {
+  it('assigns the "selected" class to elements targeted by an annotation, which got selected by a user', () => {
+    document.body.innerHTML = `
+    <div id="text"><div id="w.1"></div><div id="w.2"></div><div id="w.3"  class="selected"></div></div>`;
+    const $text = document.getElementById('text');
+    const annotation = {
+      targets: [
+        {
+          selector: {
+            value: 'id("w.1")',
+          },
+        },
+        {
+          selector: {
+            value: 'id("w.2")',
+          },
+        },
+      ],
+    };
+    const $element1 = document.getElementById('w.1');
+    const $element2 = document.getElementById('w.2');
+    const $element3 = document.getElementById('w.3');
+
+    highlightSelectedAnnotationsTarget(annotation, $text);
+
+    expect($element1.classList.contains('selected')).toBe(true);
+    expect($element2.classList.contains('selected')).toBe(true);
+    expect($element3.classList.contains('selected')).toBe(false);
+  });
+});
